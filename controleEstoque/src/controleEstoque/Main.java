4661adfb371a6bc5f4fc5c16694bae6a02d87d40
Gabriel Regel <161236355+GabrielRegel@users.noreply.github.com>
2025-08-04 12:10:37 -0300

    Add files via upload

---
+++ controleEstoque/src/controleEstoque/Main.java
@@ -0,0 +1,35 @@
+package controleEstoque;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Estoque = estoque = new Estoque();
+		int opcao;
+		
+		do {
+			System.out.println("============= MENU DO ESTOQUE =============");
+			System.out.println("1. Dar Entrada");
+			System.out.println("2. Dar Saida");
+			System.out.println("3. Cadastrar Produto");
+			System.out.println("0. Sair");
+			System.out.print("\nEscolha uma opção: ");
+			
+			opcao = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch (opcao) {
+				case 1:
+					
+				
+			}
+			
+			
+			
+		} while (opcao != sla);
+		
+		scanner.close();
+	}
+
+}
---
+++ controleEstoque/src/controleEstoque/Produto.java
@@ -0,0 +1,13 @@
+package controleEstoque;
+
+public class Produto {
+	private String nomeProduto;
+	private String numSerie;
+	private String numLote;
+	private float valor;
+	
+	
+	public Produto(String nomeProduto, String numSerie, String numLote) {
+		
+	}
+}
